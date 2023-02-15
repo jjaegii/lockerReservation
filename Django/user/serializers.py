932f70b9c0b9e7b33a5643d1a9a21ff2a4006677
jjaegii <hn06038@gmail.com>
2023-02-16 04:25:20 +0900

    reduce UserSerializer

--- Django/user/serializers.py
+++ Django/user/serializers.py
@@ -2,15 +2,7 @@ from rest_framework import serializers
 from user.models import User
 
 
-class RegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        max_length=128,
-        min_length=8,
-        write_only=True
-    )
-
-    token = serializers.CharField(max_length=255, read_only=True)
-
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
@@ -18,8 +10,4 @@ class RegistrationSerializer(serializers.ModelSerializer):
             'name',
             'phone_num',
             'password',
-            'token'
         ]
-
-    def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
