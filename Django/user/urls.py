02ab35ba31fa1d5cf4ed266c7e7880ed06ad58ae
jjaegii <hn06038@gmail.com>
2023-02-16 04:25:55 +0900

    modify register, add login, logout

--- Django/user/urls.py
+++ Django/user/urls.py
@@ -4,5 +4,6 @@ from user import views
 urlpatterns = [
     # 네이버 api 휴대폰 인증 기능, 미구현
     # path('register/', views.Auth.as_view())
-    path('register/', views.RegistrationAPIView.as_view())
+    path('register', views.registration_api),
+    path('login', views.login_api),
 ]
