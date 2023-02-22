6aa088b876adee67d59315f88b466154484a7d81
Grodned <dendrog@DESKTOP-F1P20TT.localdomain>
2023-02-22 12:05:18 +0900

    create admin page

--- Django/locker_state/urls.py
+++ Django/locker_state/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
 
     #path('state',views.LockerListAPIView.as_view()),
     path('state',views.LockerList),
+    path('state/admin',admin.site.urls),
     #path('state/<int:loc>',views.Lockerset),
 ]
--- Django/user/urls.py
+++ Django/user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from user import views
+from django.contrib import admin
 
 urlpatterns = [
     # 네이버 api 휴대폰 인증 기능, 미구현
     # path('register/', views.Auth.as_view())
     path('register', views.registration_api),
     path('login', views.login_api),
-    path('logout', views.logout_api)
+    path('logout', views.logout_api),
+    path('admin',admin.site.urls),
 ]
