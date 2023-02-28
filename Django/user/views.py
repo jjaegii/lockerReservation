836fecc309b55c728f67b8d08d5e5afe7f62dc9a
jjaegii <hn06038@gmail.com>
2023-02-28 18:11:24 +0900

    add check phone cert func in register func

--- Django/user/views.py
+++ Django/user/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 
-from user.models import User,Cert_table
+from user.models import User, Cert_table
 from lockerReservation.session import session_funcs
 from lockerReservation.phonecert import cert_funcs
 from user.serializers import CertSerializer
@@ -26,18 +26,20 @@ def phone_api(request, format=None):
             serializer = CertSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
-            cert_funcs().certification(request.data['cert_number'],request.data['phone_num'])
-            #print(type(a))
+            cert_funcs().certification(
+                request.data['cert_number'], request.data['phone_num'])
+            # print(type(a))
             return Response({"status": "ok"}, status=status.HTTP_200_OK)
     except:
         return Response({"error": ""}, status=status.HTTP_404_NOT_FOUND)
+
 
 @api_view(['POST'])
 def certcheck_api(request, format=None):
     try:
         if request.method == 'POST':
-            save = Cert_table.objects.get(phone_num = request.data['phone_num'])
-            if(save.cert_number == request.data['cert_number']):
+            save = Cert_table.objects.get(phone_num=request.data['phone_num'])
+            if (save.cert_number == request.data['cert_number']):
                 save.cert_status = True
                 save.save()
                 return Response({"cert": "success"}, status=status.HTTP_200_OK)
@@ -45,6 +47,7 @@ def certcheck_api(request, format=None):
                 return Response({"cert": "fail"}, status=status.HTTP_200_OK)
     except:
         return Response({"error": ""}, status=status.HTTP_404_NOT_FOUND)
+
 
 '''
 class Auth(APIView):
@@ -70,9 +73,14 @@ def registration_api(request):
         phone_num = data['phone_num']
         password = data['password']
         # studentID 중복 있는지 체크
-        obj = User.objects.filter(studentID=studentID)
-        if obj:
+        if User.objects.filter(studentID=studentID).exists():
             return Response({"error": "already exists"}, status=status.HTTP_409_CONFLICT)
+
+        # 휴대폰 인증 체크
+        cert = Cert_table.objects.filter(phone_num=phone_num).values()
+        if not cert or not cert[0]['cert_status']:
+            return Response({"error": "phone cert required"}, status=status.HTTP_401_UNAUTHORIZED)
+
         # password 암호화는 나중에
         User.objects.create(studentID=studentID, name=name,
                             phone_num=phone_num, password=password)
